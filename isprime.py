4258a8d4fb7bac834359b23f88fef5e08e0a6a1c
Mendel Ba <>
2021-04-18 17:18:15 +0000

    Fonction and add second file

--- isprime.py
+++ isprime.py
@@ -1,17 +1,30 @@
+import math
+
 def isPrime(a,b):
- 
-  print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
-  arePrime : False
-  i = 2
-  while i < a and a % i != 0 and i < b and b % i != 0 :
-    i = i + 1
+    if a == 2 and b == 2:
+        prime = True
+    elif a % 2 == 0 or a <= 1 and b % 2 == 0 or b <= 1 :
+        prime = False
+    else:
+        sqra = int(math.sqrt(a)) + 1
+        sqrb = int(math.sqrt(b)) + 1
 
-  if i == a and i == b:
-    arePrime = True
-    print("Les nombres ", a, b, bool(arePrime), " sont premierS ! Fantastique !")
-  else:
-    print("Un des nombres n'est pas un nombre premier. ",bool(arePrime))
-  
-  return arePrime
-    
+        for divisor in range(3, sqra, 2):
+            if a % divisor == 0 :
+                prime = False
+                break
+        else: 
+            prime = True
+
+        for divisor in range(3, sqrb, 2):
+            if b % divisor == 0 :
+                prime = False
+                break
+        else: 
+            prime = True
+
+    if prime:
+        print("Les deux nombres sont premiers ", prime)
+    else:
+        print("Un des deux nombres n'est pas premier ", prime)
